83843af43df985b308a3c7361051d0a1dae2944e
victorlhlee <victorlhlee@users.noreply.github.com>
2018-04-20 19:10:26 -1000

    Update exercise.js

--- js/exercise.js
+++ js/exercise.js
@@ -7,14 +7,14 @@ As always, should you be caught copy and pasta, the Secretary will disavow any k
 This repo will self-destruct in five seconds. Good luck!*/
 
 /*Mission 1: 
-Team Member: 
+Team Member: Camille
 
 Target the div element with the id of 'name' and change the contents to: 'Tiger Express'
 */
 
 
  /*Mission 2: 
- Team Member: 
+ Team Member: Valzey
 
  Target the div element with the id of 'motto' and change the contents to:
  'The best Chinese food to wok yo world!'
@@ -22,27 +22,27 @@ Target the div element with the id of 'name' and change the contents to: 'Tiger 
  
 
  /*Mission 3: 
- Team Member: 
+ Team Member: Matt
 
  Target the first li element and change the content to: 'Our Food'
  */
 
 
  /*Mission 4: 
- Team Member: 
+ Team Member: Erin
 
  Change the layout of the li element from block to inline.
  */
 
 
  /*Mission 5:
- Team Member: 
+ Team Member: Harsh
  Create a paragraph element, create an id of 'tangy', give it the following content: 'Orange Chicken' and append it into the div element with the id of 'specials' and change the font color to 'orangered'
  */
 
 
  /*Mission 6:
- Team Member: 
+ Team Member: Sarah
 
  Create an image element and use the following link for your source:
  http://www.quickneasyrecipes.net/wp-content/uploads/2012/09/Panda-Express-Orange-Chicken.jpg
@@ -52,14 +52,14 @@ Target the div element with the id of 'name' and change the contents to: 'Tiger 
 
  
  /*Mission 7:
- Team Member: 
+ Team Member: Doug
 
  Target the header 2 element with the id of 'story' and change the content to: 'Bringing hungry people together to share joy has been our family's inspiration.' And change the font color to 'cornflowerblue'
  */
 
 
  /*Mission 8:
- Team Member: 
+ Team Member: Cassandra
  
  Add the following link as a background image in the body element:
  'https://i.pinimg.com/originals/ab/05/e7/ab05e7969e42fea2249c18e9fe461188.jpg'
@@ -67,14 +67,14 @@ Target the div element with the id of 'name' and change the contents to: 'Tiger 
 
 
  /*Mission 9:
- Team Member:
+ Team Member: Chaz
 
  Target the div element with the id of 'locate' and change the content to: 'Find a Tiger Express in your area.'
  */
  
 
  /*Mission 10:
- Team Member:
+ Team Member: Erik
 
  Create a div element, create an id of 'designer', give it a content of: 'Copyright 2018 Gin Drinkers Association (GDA)' and append it to the footer element.
  */
